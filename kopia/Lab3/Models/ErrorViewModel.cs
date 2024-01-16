43213cacf0125e2395c4b0d468f0a44069a2aaed
Jarosław Łyczak <jaroslaw.lyczak@microsoft.wsei.edu.pl>
2024-01-16 20:11:00 +0100

    usunieta kopia

--- kopia/Lab3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
